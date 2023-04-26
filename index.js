09286c11cee404a525a1da18e118f78b84303a2c
2003aryansingh <s424.aryansingh@gmail.com>
2023-04-26 13:08:02 +0530

    Added some comments to make it more understandable

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get("/", (req,res)=>{
 
 
 app.get("/stream",(req,res)=> {
-    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Content-Type", "text/event-stream");     //response header to tell the browser that the response is Server Sent Events
     send(res);
 });
 
@@ -17,9 +17,9 @@ const port = 8080;
 let i = 0;
 
 function send(res) {
-    res.write("data: " + `hello hi how are you [${i++}]\n\n`);
+    res.write("data: " + `hello hi how are you [${i++}]\n\n`);          //sends this response every 1 second,, response never ends 
 
-    setTimeout(()=> send(res), 1000);
+    setTimeout(()=> send(res), 1000);          
 }
 
 app.listen(port, ()=> {
